74ac0a9e101929f23ce3e644b6c11514c1fec1fa
echo <tiramisu13795@gmail.com>
2023-09-17 23:39:38 +0900

    [feat]: ~section4

---
+++ src/main/java/hello/proxy/config/v1_proxy/ConcreteProxyConfig.java
@@ -0,0 +1,34 @@
+package hello.proxy.config.v1_proxy;
+
+import hello.proxy.app.v2.OrderControllerV2;
+import hello.proxy.app.v2.OrderRepositoryV2;
+import hello.proxy.app.v2.OrderServiceV2;
+import hello.proxy.config.v1_proxy.concreteproxy.OrderControllerConcreteProxy;
+import hello.proxy.config.v1_proxy.concreteproxy.OrderRepositoryConcreteProxy;
+import hello.proxy.config.v1_proxy.concreteproxy.OrderServiceConcreteProxy;
+import hello.proxy.trace.logtrace.LogTrace;
+import lombok.extern.java.Log;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConcreteProxyConfig {
+
+    @Bean
+    public OrderControllerV2 orderControllerV2(LogTrace logTrace) {
+        OrderControllerV2 controllerImpl = new OrderControllerV2(orderServiceV2(logTrace));
+        return new OrderControllerConcreteProxy(controllerImpl, logTrace);
+    }
+
+    @Bean
+    public OrderServiceV2 orderServiceV2(LogTrace logTrace) {
+        OrderServiceV2 orderServiceV2 = new OrderServiceV2(orderRepositoryV2(logTrace));
+        return new OrderServiceConcreteProxy(orderServiceV2, logTrace);
+    }
+
+    @Bean
+    public OrderRepositoryV2 orderRepositoryV2(LogTrace logTrace) {
+        OrderRepositoryV2 repositoryImpl = new OrderRepositoryV2();
+        return new OrderRepositoryConcreteProxy(repositoryImpl, logTrace);
+    }
+}
